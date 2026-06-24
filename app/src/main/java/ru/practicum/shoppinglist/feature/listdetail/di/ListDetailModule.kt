b4a429fdea03c02349802baaeebc9451be914df8
Александр <rainfire491@gmail.com>
2026-06-24 18:31:58 +1100

    remove import org.koin.core.module.dsl.viewModelOf

--- app/src/main/java/ru/practicum/shoppinglist/feature/listdetail/di/ListDetailModule.kt
+++ app/src/main/java/ru/practicum/shoppinglist/feature/listdetail/di/ListDetailModule.kt
@@ -1,6 +1,5 @@
 package ru.practicum.shoppinglist.feature.listdetail.di
 
-//import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 import ru.practicum.shoppinglist.core.data.database.AppDatabase
 import ru.practicum.shoppinglist.feature.listdetail.data.dao.ProductDao
