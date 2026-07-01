500c322b3128b4398dcf1a172f358afcd2c1efe2
elkabelaya <elkabelaya@github.com>
2026-07-01 16:56:18 +0300

    #70/ #69 T-49 — Сетевой слой: Retrofit + OkHttp + networkModule; T-50 — Auth API: DTO + AuthApi (Retrofit)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,4 +104,8 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    // Network
+    implementation(libs.retrofit)
+    implementation(libs.converter.json)
 }
