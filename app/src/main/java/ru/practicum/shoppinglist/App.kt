cbef26866cb9a7a501f0186d0f8cf8754ecd34dd
elkabelaya <elkabelaya@github.com>
2026-07-14 10:36:29 +0300

    bit fix

--- app/src/main/java/ru/practicum/shoppinglist/App.kt
+++ app/src/main/java/ru/practicum/shoppinglist/App.kt
@@ -3,22 +3,12 @@ package ru.practicum.shoppinglist
 import android.app.Application
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
-import org.koin.core.module.Module
 import ru.practicum.shoppinglist.core.di.coreModule
 import ru.practicum.shoppinglist.feature.auth.di.authModule
 import ru.practicum.shoppinglist.feature.listdetail.di.listDetailModule
 import ru.practicum.shoppinglist.feature.lists.di.listsModule
 import ru.practicum.shoppinglist.feature.onboarding.di.onboardingModule
 import ru.practicum.shoppinglist.root.di.rootModule
-
-var appModules: List<Module> = listOf(
-    coreModule,
-    rootModule,
-    authModule,
-    listDetailModule,
-    listsModule,
-    onboardingModule
-)
 
 class App : Application() {
 
@@ -27,7 +17,12 @@ class App : Application() {
         startKoin {
             androidContext(this@App)
             modules(
-                appModules
+                coreModule,
+                rootModule,
+                authModule,
+                listDetailModule,
+                listsModule,
+                onboardingModule
             )
         }
     }
